7d6b96f7295088f7f3271e32e1e98919e561620f
原田礼朗 <rharada@mba-11aj-090.local>
2016-10-19 01:07:32 +0900

    Initial Commit

---
+++ hackForWeb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackForWeb
+//
+//  Created by 原田　礼朗 on 2016/10/19.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
